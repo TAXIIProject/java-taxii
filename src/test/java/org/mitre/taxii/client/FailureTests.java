2c4e2ceb687bdb5cc559e6b602e8b3d55b7fd533
jasenj1 <jasenj1@mm190327-pc.mitre.org>
2014-08-28 09:40:38 -0400

    Test the HTTP Client.

--- src/test/java/org/mitre/taxii/client/FailureTests.java
+++ src/test/java/org/mitre/taxii/client/FailureTests.java
@@ -4,57 +4,50 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-import javax.net.ssl.SSLContext;
 import javax.xml.bind.JAXBException;
-import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContexts;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.mitre.taxii.Messages;
 import org.mitre.taxii.messages.xml11.DiscoveryRequest;
-import org.mitre.taxii.messages.xml11.DiscoveryResponse;
 import org.mitre.taxii.messages.xml11.ObjectFactory;
+import org.mitre.taxii.messages.xml11.StatusMessage;
 import org.mitre.taxii.messages.xml11.TaxiiXml;
 import org.mitre.taxii.messages.xml11.TaxiiXmlFactory;
+import org.mitre.taxii.query.DefaultQuery;
 
 /**
  *
  * @author jasenj1
  */
-public class DiscoveryTests {
-
+public class FailureTests {
+    
     private final ObjectFactory factory = new ObjectFactory();
     private final TaxiiXmlFactory txf = new TaxiiXmlFactory();
     private final TaxiiXml taxiiXml;
     private final boolean debug = true; // Boolean.getBoolean("debug");
 
-    public DiscoveryTests() {
+    public FailureTests() {
         taxiiXml = txf.getTaxiiXml();
     }
-   
+    
     @Test
-    public void simpleDiscoveryTest() throws IOException, JAXBException, URISyntaxException {
+    public void missingEndpointDiscoveryTest() throws IOException, JAXBException, URISyntaxException {
         org.mitre.taxii.client.xml11.HttpClient taxiiClient = new org.mitre.taxii.client.xml11.HttpClient(taxiiXml);
-        final String serverUrl = "http://127.0.0.1:8000/services/discovery/";
+        final String serverUrl = "http://127.0.0.1:8000/services/discover/"; // URL does not exist.
 
         // Prepare the message to send.
         DiscoveryRequest dr = factory.createDiscoveryRequest()
                 .withMessageId(Messages.generateMessageId());
 
-        // Call the services
         Object responseObj = taxiiClient.callTaxiiService(new URI(serverUrl), dr);
 
         if (debug) {
@@ -62,29 +55,27 @@ public class DiscoveryTests {
             System.out.println(taxiiXml.marshalToString(responseObj, true));
         }
 
-        assertTrue("Received Discovery Response", (responseObj instanceof DiscoveryResponse));
+        assertTrue("Received Status Message", (responseObj instanceof StatusMessage));
     }
 
     @Test
-    public void basicAuthDiscoveryTest() throws IOException, JAXBException, URISyntaxException {
+    public void failBasicAuthDiscoveryTest() throws IOException, JAXBException, URISyntaxException {
         
-        // Create a client that uses basic authentication (user & password).
+        // Create a client that uses basic (user & password) authentication.
         HttpClientBuilder cb = HttpClientBuilder.create();
         CredentialsProvider credsProvider = new BasicCredentialsProvider();
         credsProvider.setCredentials(
                 AuthScope.ANY,
-                new UsernamePasswordCredentials("taxii", "taxii"));        
+                new UsernamePasswordCredentials("taxii", "taxi")); // Bad password
         cb.setDefaultCredentialsProvider(credsProvider);        
         CloseableHttpClient httpClient = cb.build();
 
-        // Create a Taxii Client with the HttpClient and TaxiiXml objects.
         org.mitre.taxii.client.xml11.HttpClient taxiiClient = new org.mitre.taxii.client.xml11.HttpClient(taxiiXml, httpClient);
 
         // Prepare the message to send.
         DiscoveryRequest dr = factory.createDiscoveryRequest()
                 .withMessageId(Messages.generateMessageId());
 
-        // Call the service
         final String serverUrl = "http://127.0.0.1:8100/services/discovery/";
         Object responseObj = taxiiClient.callTaxiiService(new URI(serverUrl), dr);
 
@@ -93,14 +84,14 @@ public class DiscoveryTests {
             System.out.println(taxiiXml.marshalToString(responseObj, true));
         }
 
-        assertTrue("Received Discovery Response", (responseObj instanceof DiscoveryResponse));
+        assertTrue("Received Status Message", (responseObj instanceof StatusMessage));
     }
-    
+
     @Test
-    public void sslDiscoveryTest() throws IOException, JAXBException, KeyStoreException, NoSuchAlgorithmException, CertificateException, KeyManagementException, URISyntaxException {
+    public void failedSslDiscoveryTest() throws IOException, JAXBException, KeyStoreException, NoSuchAlgorithmException, CertificateException, KeyManagementException, URISyntaxException {
         final String serverUrl = "https://127.0.0.1:8443/services/discovery/";
 
-        // Prepare the TLS objects necessary for SSL connection.
+        /* The below configures the client to accept self-signed certificates.
         KeyStore trustStore  = KeyStore.getInstance(KeyStore.getDefaultType());
         trustStore.load(null, "nopassword".toCharArray());
 
@@ -117,50 +108,25 @@ public class DiscoveryTests {
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setSSLSocketFactory(sslsf)
                 .build();        
+        */
         
-        // Create a Taxii Client with the HttpClient and TaxiiXml objects.
-        org.mitre.taxii.client.xml11.HttpClient taxiiClient = new org.mitre.taxii.client.xml11.HttpClient(taxiiXml, httpClient);
+        org.mitre.taxii.client.xml11.HttpClient taxiiClient = new org.mitre.taxii.client.xml11.HttpClient(taxiiXml);
 
         // Prepare the message to send.
         DiscoveryRequest dr = factory.createDiscoveryRequest()
                 .withMessageId(Messages.generateMessageId());
 
-        // Call the service
-        Object responseObj = taxiiClient.callTaxiiService(new URI(serverUrl), dr);
+        Object responseObj = null;
+        responseObj = taxiiClient.callTaxiiService(new URI(serverUrl), dr);
 
         if (debug) {
             System.out.println(taxiiXml.marshalToString(dr, true));
-            System.out.println(taxiiXml.marshalToString(responseObj, true));
+            if (null != responseObj) {
+                System.out.println(taxiiXml.marshalToString(responseObj, true));
+            }
         }
 
-        assertTrue("Received Discovery Response", (responseObj instanceof DiscoveryResponse));
+        assertTrue("Received Status Message", (responseObj instanceof StatusMessage));
     }
-        
-    @Test
-    public void proxyDiscoveryTest() throws IOException, JAXBException, URISyntaxException {
-        
-        // Create a client that connects via a proxy.
-        HttpHost proxy = new HttpHost("gatekeeper-w.mitre.org", 80, "http");        
-        HttpClientBuilder cb = HttpClientBuilder.create();        
-        cb.setProxy(proxy);        
-        CloseableHttpClient httpClient = cb.build();
-
-        // Create a Taxii Client with the HttpClient and TaxiiXml objects.
-        org.mitre.taxii.client.xml11.HttpClient taxiiClient = new org.mitre.taxii.client.xml11.HttpClient(taxiiXml, httpClient);
-        
-        // Prepare the message to send.
-        DiscoveryRequest dr = factory.createDiscoveryRequest()
-                .withMessageId(Messages.generateMessageId());
-
-        // Call the service
-        final String serverUrl = "http://taxiitest.mitre.org/services/discovery/";
-        Object responseObj = taxiiClient.callTaxiiService(new URI(serverUrl), dr);
-
-        if (debug) {
-            System.out.println(taxiiXml.marshalToString(dr, true));
-            System.out.println(taxiiXml.marshalToString(responseObj, true));
-        }
-
-        assertTrue("Received Discovery Response", (responseObj instanceof DiscoveryResponse));
-    }       
+    
 }
--- src/test/java/org/mitre/taxii/client/PollRequestTest.java
+++ src/test/java/org/mitre/taxii/client/PollRequestTest.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.mitre.taxii.client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.xml.bind.JAXBException;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.mitre.taxii.Messages;
+import org.mitre.taxii.messages.xml11.ObjectFactory;
+import org.mitre.taxii.messages.xml11.PollRequest;
+import org.mitre.taxii.messages.xml11.PollResponse;
+import org.mitre.taxii.messages.xml11.TaxiiXml;
+import org.mitre.taxii.messages.xml11.TaxiiXmlFactory;
 
 /**
  *
  * @author jasenj1
  */
 public class PollRequestTest {
+    private final ObjectFactory factory = new ObjectFactory();
+    private final TaxiiXmlFactory txf = new TaxiiXmlFactory();
+    private final TaxiiXml taxiiXml;
+    private final boolean debug = true; // Boolean.getBoolean("debug");
+
+    public PollRequestTest() {
+        taxiiXml = txf.getTaxiiXml();
+    }
     
+    @Test
+    public void pollRequest() throws URISyntaxException, JAXBException, IOException {
+        org.mitre.taxii.client.xml11.HttpClient taxiiClient = new org.mitre.taxii.client.xml11.HttpClient(taxiiXml);
+        final URI serviceUri = new URI("http://127.0.0.1:8000/services/poll/");
+        
+        PollRequest pr = factory.createPollRequest()
+                        .withMessageId(Messages.generateMessageId())
+                        .withCollectionName("default")
+                        .withPollParameters(
+                                factory.createPollParametersType()
+                        );
+        
+        Object responseObj = taxiiClient.callTaxiiService(serviceUri, pr);
+
+        if (debug) {
+            System.out.println(taxiiXml.marshalToString(pr, true));
+            System.out.println(taxiiXml.marshalToString(responseObj, true));
+        }
+        assertTrue("Received Poll Response", (responseObj instanceof PollResponse));        
+    }    
 }
